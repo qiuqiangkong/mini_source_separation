f4166fc013c9e6df0c30f92e44a7ad51d001428a
Qiuqiang Kong <qiuqiangkong@gmail.com>
2024-10-08 11:54:05 +0800

    update

---
+++ utils.py
@@ -0,0 +1,16 @@
+import torch
+from collections import OrderedDict
+
+
+@torch.no_grad()
+def update_ema(ema_model, model, decay=0.999):
+    ema_params = OrderedDict(ema_model.named_parameters())
+    model_params = OrderedDict(model.named_parameters())
+
+    for name, param in model_params.items():
+        ema_params[name].mul_(decay).add_(param.data, alpha=1 - decay)
+
+
+def requires_grad(model, flag=True):
+    for p in model.parameters():
+        p.requires_grad = flag
